5d3c60ebec54ff3614b93532733eaaaf46cbc267
Sam Richard <sam.richard@us.ibm.com>
2016-01-21 13:39:44 -0500

    Change test to promise

--- tests/meta.js
+++ tests/meta.js
@@ -3,13 +3,12 @@
 import test from 'ava';
 import meta from '../lib/meta';
 import process from 'child_process';
+import Promise from 'bluebird';
 
 test('get packages', t => {
   return meta('..').then(modules => {
-    process.exec('npm ls --json --depth=0 --long', function (err, stdout) {
-      if (err) {
-        t.fail(err);
-      }
+    var exec = Promise.promisify(process.exec);
+    return exec('npm ls --json --depth=0 --long').then(function (stdout) {
       var expected = JSON.parse(stdout);
 
       // Same number of items returned as are available at to-level node
