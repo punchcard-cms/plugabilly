691f3588c2334819e1bf53a23c7c4f2f8a8c83a8
Sam Richard <sam.richard@us.ibm.com>
2016-01-21 13:21:55 -0500

    Update META test to work for NPM 2, 3, and beyond

--- tests/meta.js
+++ tests/meta.js
@@ -2,21 +2,25 @@
 
 import test from 'ava';
 import meta from '../lib/meta';
-import path from 'path';
-import glob from 'glob';
+import process from 'child_process';
 
 test('get packages', t => {
   return meta('..').then(modules => {
-    var expected = glob.sync('../node_modules/*').length;
+    process.exec('npm ls --json --depth=0 --long', function (err, stdout) {
+      if (err) {
+        t.fail(err);
+      }
+      var expected = JSON.parse(stdout);
 
-    // Same number of items returned as are available at to-level node
-    t.same(modules.length, expected);
+      // Same number of items returned as are available at to-level node
+      t.same(modules.length, Object.keys(expected.dependencies).length);
 
-    // Each module has path, package, and package's name
-    modules.forEach(function (module) {
-      t.true(module.hasOwnProperty('path'));
-      t.true(module.hasOwnProperty('package'));
-      t.true(module.package.hasOwnProperty('name'));
+      // Each module has path, package, and package's name
+      modules.forEach(function (module) {
+        t.true(module.hasOwnProperty('path'));
+        t.true(module.hasOwnProperty('package'));
+        t.true(module.package.hasOwnProperty('name'));
+      });
     });
   });
 });
